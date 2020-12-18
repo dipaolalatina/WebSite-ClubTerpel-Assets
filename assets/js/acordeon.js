d774d18312fe7d3a9aeb50b5c5416595de2769be
Diego Araya <diego@dipaola.cl>
2020-12-18 11:40:20 -0300

    faq 1

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
 
     var allPanels = $('.accordion > dl > dd').hide();
-                  var allPanels2 = $('.accordion > dl > dt > a');
-                  allPanels2.addClass('accordion-title is-collapsed');
-                     
-                   $('.accordion > dl > dt > a').on( "click", function() {
-                     allPanels.slideUp();
-                     $(this).parent().next().slideDown();
-                     allPanels2.removeClass('is-expanded');
-                     allPanels2.addClass('is-collapsed');
-                     $(this).removeClass('is-collapsed');
-                     $(this).addClass('is-expanded');
-                     return false;
-                   });
+        var allPanels2 = $('.accordion > dl > dt > a');
+        allPanels2.addClass('accordion-title is-collapsed');
+            
+        $('.accordion > dl > dt > a').on( "click", function() {
+            allPanels.slideUp();
+            $(this).parent().next().slideDown();
+            allPanels2.removeClass('is-expanded');
+            allPanels2.addClass('is-collapsed');
+            $(this).removeClass('is-collapsed');
+            $(this).addClass('is-expanded');
+            return false;
+        });
 });
